16b969891b996796651f2db992b8b90c0304784e
Bruce Armstrong <bruce.armstrong09@gmail.com>
2019-11-02 21:38:47 +0530

    Initial Commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1padsbhf3ce_in(uj9ya@attqfwa!(9cjhaefyxjibx=qxjfc2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mysite.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,9 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
 
 
 STATICFILES_DIRS = [
